253ed6f81f04a087f99eab6b5eb1207e543bc2e5
nkthiebaut <nkthiebaut@gmail.com>
2018-01-21 00:27:25 -0800

    :wrench: adapt setup script to latest version (0.12)

--- setup.py
+++ setup.py
@@ -3,16 +3,11 @@
 from distutils.core import setup
 
 setup(name='zeugma',
-      version='0.11',
+      version='0.12',
       description='Unified framework for word embeddings (Word2Vec, GloVe, FastText, ...) compatible with scikit-learn Pipeline',
       author='Nicolas Thiebaut',
       author_email='nkthiebaut@gmail.com',
       url='https://github.com/nkthiebaut',
-      download_url='https://github.com/nkthiebaut/zeugma/archive/0.11.tar.gz',
-      install_requires=[
-          'numpy>=0.1',
-	  'pandas',
-          'sklearn',
-      ]
+      download_url='https://github.com/nkthiebaut/zeugma/archive/0.12.tar.gz',
      )
 
