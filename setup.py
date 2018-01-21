0d0b94332728d63566c6bddffd319ae45138102e
nkthiebaut <nkthiebaut@gmail.com>
2018-01-21 01:18:32 -0800

    :wrench: fix typo in setup script...

--- setup.py
+++ setup.py
@@ -4,21 +4,23 @@ from distutils.core import setup
 
 setup(name='zeugma',
       packages=['zeugma'],
-      version='0.16',
+      version='0.17',
       license='MIT',
       description='Unified framework for word embeddings (Word2Vec, GloVe, FastText, ...) compatible with scikit-learn Pipeline',
       author='Nicolas Thiebaut',
       author_email='nkthiebaut@gmail.com',
       url='https://github.com/nkthiebaut',
-      download_url='https://github.com/nkthiebaut/zeugma/archive/0.16.tar.gz',
+      download_url='https://github.com/nkthiebaut/zeugma/archive/0.17.tar.gz',
       keywords=['embeddings'],
       classifiers=[],
+      setup_requires=[
+          'numpy>=1.13.3',
+          'Cython',
+      ]
       install_requires=[
           'pandas>=0.20.3',
-          'numpy>=1.13.3',
           'gensim>=3.2.0',
           'scikit_learn>=0.19.1',
-          'Cython',
           'fastText',
       ],
       dependency_links=['git+git://github.com/facebookresearch/fastText.git']
