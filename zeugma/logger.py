1fa58dec550fe5e38686fd2ad177c5e5e8950caf
nkthiebaut <nkthiebaut@gmail.com>
2018-08-22 10:27:13 -0700

    :sparkles: add LGTM code quality inspection and badge

--- zeugma/logger.py
+++ zeugma/logger.py
@@ -4,20 +4,17 @@ Created on the 01/25/2018
 @author: Nicolas Thiebaut
 @email: nicolas@visage.jobs
 """
-import logging
-
-from logging import StreamHandler
-from logging import Formatter
+from logging import Formatter, getLogger, StreamHandler, WARNING
 
 LOG_FORMAT = (
     "%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno)d")
 
-DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_LOG_LEVEL = WARNING
 
 
 DEFAULT_HANDLER = StreamHandler()
 DEFAULT_HANDLER.setFormatter(Formatter(LOG_FORMAT))
 
-package_logger = logging.getLogger("zeugma")
+package_logger = getLogger("zeugma")
 package_logger.setLevel(DEFAULT_LOG_LEVEL)
 package_logger.addHandler(DEFAULT_HANDLER)
