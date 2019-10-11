764063f4c2f56a560d52f1a52a7965aa7d71252e
Nicolas Thiebaut <nicolas.thiebaut@hired.com>
2019-10-10 18:56:08 -0700

    :construction_worker: add support for python 3.6 and 3.7 in travis

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ htmlhelp_basename = "zeugmadoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements: dict = {
+latex_elements = {  # type: ignore
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
